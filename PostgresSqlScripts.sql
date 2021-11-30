01d59932204f791cf39cfc6c94ca57e5ca757fa6
Arun Koshy <40599502+ArunKoshy07@users.noreply.github.com>
2021-11-30 23:15:49 +0000

    updated scripts

--- PostgresSqlScripts.sql
+++ PostgresSqlScripts.sql
@@ -1,5 +1,25 @@
--- 1. List countries with income level of "Upper middle income"
-select * from public."worldBankGdpCnty" where "incomeLevelid" = 'UMC'
+-- 1. List of countries with income level of "Upper middle income"
+select "name" as "Country Name","adminregionvalue" as "Region", "incomeLevelvalue" as "Income Level" 
+from public."worldBankGdpCnty" where "incomeLevelid" = 'UMC'
+
+-- 2. List of countries with income level of "Low income" per region.
+select "regionid", "adminregionvalue" as "Region", "name" as "Country Name", 
+"incomeLevelvalue" as "Income Level" 
+from public."worldBankGdpCnty" 
+where "incomeLevelid" = 'LIC'
+group by (regionid, adminregionvalue,"name", "incomeLevelvalue")
+order by regionid
 
 
-select * from public."worldBankGdpRegn"
+-- 3. The region with the highest proportion of "High income" countries.
+
+select rslt.name as "Region",rslt.noofcountries "No of countries in High Income level",rslt.region_rank as "Region Rank" from(
+select cnty.regionid,cnty.noofcountries, RANK() OVER( ORDER BY cnty.noofcountries DESC) as "region_rank" ,reg.name 
+from (
+select regionid,count(name) as noofcountries
+from public."worldBankGdpCnty" 
+where "incomeLevelid" = 'HIC'
+group by (regionid)) cnty
+LEFT JOIN public."worldBankGdpRegn" as reg on reg.id = cnty.regionid ) rslt
+where rslt.region_rank = 1;
+
