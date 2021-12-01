f5d33a09fe27fd356f363f33218ccc84e9ae7585
Arun Koshy <40599502+ArunKoshy07@users.noreply.github.com>
2021-12-01 00:13:50 +0000

    Updates on 5qs

--- PostgresSqlScripts.sql
+++ PostgresSqlScripts.sql
@@ -23,3 +23,17 @@ group by (regionid)) cnty
 LEFT JOIN public."worldBankGdpRegn" as reg on reg.id = cnty.regionid ) rslt
 where rslt.region_rank = 1;
 
+-- 4. Calculate cumulative/running value of GDP per region ordered by income from lowest to highest and country name.
+
+
+--- 5 Calculate percentage difference in value of GDP year-on-year per country
+
+select "CountryName", "CountryCode",
+(("2019"-"2018")*100)/(CASE WHEN "2018"=0 THEN 1 ELSE "2018" END) as "YoY%Change2019",
+(("2020"-"2019")*100)/(CASE WHEN "2019"=0 THEN 1 ELSE "2019" END) as "YoY%Change2020",
+(("2021"-"2020")*100)/(CASE WHEN "2020"=0 THEN 1 ELSE "2020" END) as "YoY%Change2021",
+(("2022"-"2021")*100)/(CASE WHEN "2021"=0 THEN 1 ELSE "2021" END) as "YoY%Change2022" 
+from
+(select "CountryName", "CountryCode", "IndicatorName","IndicatorCode","2018","2019","2020","2021","2022"
+from public."worldBankDataCatalogueGep" 
+where "CountryCode" <> ALL (array['AME','EAA','EMD','E19','ECH','LAP','MNH','SAP','SSP','WLT'])) rslt
