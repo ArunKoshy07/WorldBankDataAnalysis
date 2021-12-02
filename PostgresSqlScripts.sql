0fd0ed1999565e78cb6d762dd674d1ca9ac9e2e7
Arun Koshy <arun.koshy.04@gmail.com>
2021-12-02 00:07:50 +0000

    updated files

--- PostgresSqlScripts.sql
+++ PostgresSqlScripts.sql
@@ -1,8 +1,8 @@
--- 1. List of countries with income level of "Upper middle income"
+-- 1. List countries with income level of "Upper middle income"
 select "name" as "Country Name","adminregionvalue" as "Region", "incomeLevelvalue" as "Income Level" 
 from public."worldBankGdpCnty" where "incomeLevelid" = 'UMC'
 
--- 2. List of countries with income level of "Low income" per region.
+-- 2. List countries with income level of "Low income" per region.
 select "regionid", "adminregionvalue" as "Region", "name" as "Country Name", 
 "incomeLevelvalue" as "Income Level" 
 from public."worldBankGdpCnty" 
@@ -11,7 +11,7 @@ group by (regionid, adminregionvalue,"name", "incomeLevelvalue")
 order by regionid
 
 
--- 3. The region with the highest proportion of "High income" countries.
+-- 3. Find the region with the highest proportion of "High income" countries.
 
 select rslt.name as "Region",rslt.noofcountries "No of countries in High Income level",rslt.region_rank as "Region Rank" from(
 select cnty.regionid,cnty.noofcountries, RANK() OVER( ORDER BY cnty.noofcountries DESC) as "region_rank" ,reg.name 
@@ -23,10 +23,13 @@ group by (regionid)) cnty
 LEFT JOIN public."worldBankGdpRegn" as reg on reg.id = cnty.regionid ) rslt
 where rslt.region_rank = 1;
 
+
+
 -- 4. Calculate cumulative/running value of GDP per region ordered by income from lowest to highest and country name.
 
 
 --- 5 Calculate percentage difference in value of GDP year-on-year per country
+
 
 select "CountryName", "CountryCode",
 (("2019"-"2018")*100)/(CASE WHEN "2018"=0 THEN 1 ELSE "2018" END) as "YoY%Change2019",
@@ -37,3 +40,25 @@ from
 (select "CountryName", "CountryCode", "IndicatorName","IndicatorCode","2018","2019","2020","2021","2022"
 from public."worldBankDataCatalogueGep" 
 where "CountryCode" <> ALL (array['AME','EAA','EMD','E19','ECH','LAP','MNH','SAP','SSP','WLT'])) rslt
+
+
+--- 6 List 3 countries with lowest GDP per region.
+
+select "CountryName", "CountryCode",
+(("2019"-"2018")*100)/(CASE WHEN "2018"=0 THEN 1 ELSE "2018" END) as "YoY%Change2019",
+(("2020"-"2019")*100)/(CASE WHEN "2019"=0 THEN 1 ELSE "2019" END) as "YoY%Change2020",
+(("2021"-"2020")*100)/(CASE WHEN "2020"=0 THEN 1 ELSE "2020" END) as "YoY%Change2021",
+(("2022"-"2021")*100)/(CASE WHEN "2021"=0 THEN 1 ELSE "2021" END) as "YoY%Change2022" 
+from
+(select "CountryName", "CountryCode", "IndicatorName","IndicatorCode","2018","2019","2020","2021","2022"
+from public."worldBankDataCatalogueGep" 
+where "CountryCode" <> ALL (array['AME','EAA','EMD','E19','ECH','LAP','MNH','SAP','SSP','WLT'])) rslt
+
+
+---------------------------
+
+select * from public."worldBankGdpRegn" where "id"='AME'
+
+select * from public."worldBankGdpCnty" 
+
+select * from public."worldBankDataCatalogueGep"
--- extractGdpData.py
+++ extractGdpData.py
@@ -1,106 +1,154 @@
-import requests
-import json
+import requests, os
 from sqlalchemy import create_engine, exc
 import pandas as pd
 from configparser import ConfigParser
 import psycopg2
+import urllib
+import zipfile
 
 
 def get_api_results(endpoint):
- 
-    response = requests.get(endpoint,timeout=20)
+    """
+    Function to query a api endpoint and converts the results to a json.
+    the json data is converted to pandas dataframe before its returned.
+
+    params:
+      endpoint (String) - the api endpoint link
+    return:
+      Dataframe (pandas dataframe) - returns the api results as pandas dataframe.
+    """
+    response = requests.get(endpoint, timeout=20)
     response = response.json()
     return_lst = response[1]
 
-    while response[0]['page']<response[0]['pages']:
+    while response[0]["page"] < response[0]["pages"]:
         endpoint_pg = f"{endpoint}&page={int(response[0]['page'])+1}"
-        response = requests.get(endpoint_pg,timeout=20)
+        response = requests.get(endpoint_pg, timeout=20)
         response = response.json()
         return_lst = return_lst + response[1]
     return pd.json_normalize(return_lst)
 
-def read_csv(filename):
-    return pd.read_csv(filename)
 
-def get_config(filename='database.ini', section='postgresql'):
-    # create a parser
+def get_csv_data(csv_endpoint, filename):
+    """
+    Function downloads a zip file from the api endpoint. The zip file is extracted and the files are stored to
+    a local folder. The requested csv file is read and is converted to a pandas dataframe before returning it.
+
+    params:
+      csv_endpoint (String) - the api endpoint link for the zip file
+      filename (String)     - csv filename in the format <filename>.csv
+    return:
+      Dataframe (pandas dataframe) - returns the csv data as a pandas dataframe.
+    """
+    zip_path, _ = urllib.request.urlretrieve(csv_endpoint)
+    with zipfile.ZipFile(zip_path, "r") as f:
+        f.extractall("./data")
+
+    return pd.read_csv(f"./data/{filename}")
+
+
+def get_db_engine(filename="database.ini", section="postgresql"):
+    """
+    This function reads database properties file and extracts the postgres db properties. It then creates
+    a database engine using these properties and returns the same.
+
+    params
+      filename (String) - database properties file (default value is database.ini)
+      section (String) - its the section in the properties file that needs to be read (default value is postgresql)
+    return
+      database engine  - returns a database engine object.
+    """
     parser = ConfigParser()
+
     # read config file
     parser.read(filename)
 
     # get section, default to postgresql
-    db = {}
+    params = {}
     if parser.has_section(section):
-        params = parser.items(section)
-        for param in params:
-            db[param[0]] = param[1]
+        params_ext = parser.items(section)
+        for param in params_ext:
+            params[param[0]] = param[1]
     else:
-        raise Exception('Section {0} not found in the {1} file'.format(section, filename))
+        raise Exception(
+            "Section {0} not found in the {1} file".format(section, filename)
+        )
 
-    return db
-
-
-
-def connect_db():
-    """ Connect to the PostgreSQL database server """
-    conn = None
+    # intialise the engine variable
     engine = create_engine
+
     try:
-        # read connection parameters
-        params = get_config()
-        
+
         connect = "postgresql+psycopg2://%s:%s@%s:5432/%s" % (
-                    params['user'],
-                    params['password'],
-                    params['host'],
-                    params['database']
-                    )
+            params["user"],
+            params["password"],
+            params["host"],
+            params["database"],
+        )
         engine = create_engine(connect)
     except (exc.SQLAlchemyError) as error:
         print(error)
 
     return engine
 
-def write_to_db(df,engine,tableName):
-    df.to_sql(
-        tableName, 
-        con=engine, 
-        index=False, 
-        if_exists='replace'
-    )
 
-def main():
-    worldbank_gdp_df = get_api_results("http://api.worldbank.org/v2/country?format=json")
-    worldbank_catl_gep_df = read_csv("./Data/GEPData.csv")
+def write_to_db(df, engine, tableName):
+    """
+    This function writes the pandas dataframe to the database using the db engine object passed as a
+    parameter. The table is replaced evertime its called.
+
+    parameters:
+      df (pandas dataframe) : dataframe to be written to the database
+      engine(db engine)     : db engine object to write to the database
+      tableName(String)     : table name
+    """
+    df.to_sql(tableName, con=engine, index=False, if_exists="replace")
+
+
+def main(gdp_api, catl_api, catl_file):
+    """
+    This is the main funtion which does the data extraction, transformation and loading to database.
+    it call various other sub functions to carry these steps.
+
+    parameters:
+      gdp_api (endpoint)   : endpoint to world bank gdp API
+      catl_api(endpoint)   : endpoint to world bank catalogue data.
+      catl_file(String)    : world bank catalogue data file name.
+    """
+
+    # Extract the datasets from the API endpoints and conver them to pandas dataframe
+    worldbank_gdp_df = get_api_results(gdp_api)
+    worldbank_catl_gep_df = get_csv_data(catl_api, catl_file)
+
+    # format the dataframe column names to remove spaces and dots.
     worldbank_gdp_df.columns = worldbank_gdp_df.columns.str.replace(".", "", regex=True)
-    worldbank_catl_gep_df.columns = worldbank_catl_gep_df.columns.str.replace(' ','')
+    worldbank_catl_gep_df.columns = worldbank_catl_gep_df.columns.str.replace(" ", "")
 
-    worldbank_gdp_df.to_csv('./Data/worldbankgdp.csv')          
+    # write the gdp data to the local folder
+    worldbank_gdp_df.to_csv("./data/worldbankgdp.csv")
 
-    worldbank_gdp_cnty_df = worldbank_gdp_df.loc[worldbank_gdp_df['regionid']!='NA']
-    worldbank_gdp_regn_df = worldbank_gdp_df.loc[worldbank_gdp_df['regionid']=='NA']
-     
-    engine = connect_db()
-    
-    worldbank_gdp_cnty_df.to_sql(
-        'worldBankGdpCnty', 
-        con=engine, 
-        index=False, 
-        if_exists='replace'
-    )
-    
-    worldbank_gdp_regn_df.to_sql(
-        'worldBankGdpRegn', 
-        con=engine, 
-        index=False, 
-        if_exists='replace'
-    )
-    worldbank_catl_gep_df.to_sql(
-        'worldBankDataCatalogueGep', 
-        con=engine, 
-        index=False, 
-        if_exists='replace'
-    )
+    # Split the the gdp data into countries and regions.
+    worldbank_gdp_cnty_df = worldbank_gdp_df.loc[worldbank_gdp_df["regionid"] != "NA"]
+    worldbank_gdp_regn_df = worldbank_gdp_df.loc[worldbank_gdp_df["regionid"] == "NA"]
+
+    # Create a postgres db engine using the properties in the database.ini file
+    engine = get_db_engine()
+
+    # write the dataframes to the postgres database.
+    write_to_db(worldbank_gdp_cnty_df, engine, "worldBankGdpCnty")
+    write_to_db(worldbank_gdp_regn_df, engine, "worldBankGdpRegn")
+    write_to_db(worldbank_catl_gep_df, engine, "worldBankDataCatalogueGep")
+    print("Data Successfully extracted, transformed and loaded to the database")
+
 
 if __name__ == "__main__":
-    main()
+
+    # Get the endpoints and filename from the terminal
+    gdp_api = os.getenv("WB_GDP_API", "http://api.worldbank.org/v2/country?format=json")
+    catl_api = os.getenv(
+        "WB_CATL_API", "https://databank.worldbank.org/data/download/GEP_CSV.zip"
+    )
+    catl_file = os.getenv("WB_CATL_FILE", "GEPData.csv")
+
+    # call the main funtion to extract, transform and write the data to the database
+    main(gdp_api, catl_api, catl_file)
